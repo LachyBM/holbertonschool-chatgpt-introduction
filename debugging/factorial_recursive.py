f6e1d25722b4fb78edad8d6599fac781c873bf9b
LachyBM <13072@holbertonstudents.com>
2026-05-05 02:01:30 +0000

    fixed indentation and added comment

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -2,6 +2,15 @@
 import sys
 
 def factorial(n):
+    """
+    Calculate the factorial number using recursion
+
+    Parameters:
+    a (int): a non-negative interger whose factorial is to be calced
+
+    Return:
+    int: the factorial of the given number
+    """
     if n == 0:
         return 1
     else:
